d27235130220594757699fac8d9b7069ac9c56cc
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-02 10:10:02 -0500

    worked in unsolved activities 2,4,6,8,10 for module 3 and added solutions for those activities

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,18 @@
 // Write Your JavaScript Code Here
+var personName;
+var pets;
+var funFact;
+
+personName = "Tonya";
+pets = 3;
+
+personName = "Molly";
+pets = 4;
+
+console.log("My name is " + personName + " and I have " + pets + "pets");
+
+// console.log("I have " + pets + " pets");
+
+
+
+

aea41a4f95bae06e6a04abb5107ba2f6bbdc372d
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-07 18:13:18 -0500

    added module 4 Web APIs

--- 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
+++ 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
@@ -5,12 +5,13 @@ var losses = 0;
 var ties = 0;
 
 function playRound() {
+    
 // options for choices
 var choices = ["R", "P", "S"]
 
 // ask the user to input their choice
-var userChoice = window.prompt("Enter R, P, or S");
-console.log(userChoice);
+var userChoice = prompt("Enter R, P, or S");
+console.log("User:" + userChoice);
 
 
 // firgure out the computer's choice
@@ -30,7 +31,6 @@ console.log("Comp:" +computerChoice);
 
 // compare choices and increment wins/losses/ties variable. aka compare computer and human choices and add one to wins/losses/ties variable.
     if (userChoice == computerChoice){
-        alert("tie!");
         ties = ties+1;
     }
     else if(userChoice=="R"&&computerChoice=="P") {
@@ -45,7 +45,7 @@ console.log("Comp:" +computerChoice);
     else {
         wins = wins+1;
     }
-    alert("wins"+wins+" ...ties:"+ties+"...losses:"+losses);
+    alert("wins" + wins + " ...ties:" + ties + "...losses:" + losses);
 
 
 // ...play again?

7a532535d294783e8ef17b8c3819cd072b5cd6ae
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-13 15:15:03 -0500

    added coding drill for class in Practice Example 2 folder for max number from an array

---
+++ Personal-Practice-Examples/Practice 2/assets/js/script.js
@@ -0,0 +1,26 @@
+
+// Write a function that takes in an array of numbers and outputs the maximum number. For example...
+// Input: [ 1, 2, 3 ]
+// Output: 3
+function maxOutputFirst () {
+   console.log(Math.max (1, 3, 2));
+}
+
+maxOutputFirst();
+
+
+// Input: [ 3, 6, 4, 5, 2, 1 ]
+// Output: 6
+function maxOutputSecond () {
+   console.log(Math.max (3, 6, 4, 5, 2, 1));
+}
+
+maxOutputSecond();
+
+// Input: [ 3, 3, 3 ]
+// Output: 3
+function maxOutputThird () {
+   console.log(Math.max (3, 3, 3));
+}
+
+maxOutputThird();

45659297cb444412eff77c8f784abe284b39b001
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-16 14:32:21 -0500

    added solutions to module 5 for activities 2,4,6,8,10

--- 04-Web-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
+++ 04-Web-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
@@ -3,11 +3,11 @@ var timerEl = document.querySelector('.timer-count');
 // timerEl.value = "test";
 
 function countdown () {
-    var timeleft = 10;
+    var timeLeft = 10;
 }
 
 var timeInterval = setInterval (function() {
-    if (timeleft === 0) {
+    if (timeLeft === 0) {
         clearInterval(timeInterval);
         displayAnswer();
     } 1000;

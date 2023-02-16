780e6fe9a5072cd8a9de38b60ea98f9ea990c016
mackenzie-wentworth <mwentworth28@gmail.com>
2023-02-16 10:47:54 -0500

    updated INS notes in activity 07-module 5

--- 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -2,5 +2,16 @@ var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit(event){
+    event.preventDefault();
 
+    var newItem = $("#shopping-input").val();
+    shoppingListEl.append("<li>"+newItem+"/li>");
+}
+
+
+// $.each(shoppingListEl, function () {
+//     shoppingListEl.push($(input).val());
+// })
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on("submit",handleFormSubmit);
